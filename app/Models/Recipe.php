4d08655765588cb99b34513e7cbc37a8524e1f06
Chaitanya Kuber <chaitanya.kuber@bigcommerce.com>
2014-05-18 12:23:39 +1000

    fixtures and a basic recipe model and basic test

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,5 +4,23 @@ namespace Models;
 
 class Recipe
 {
+    private $name;
+    private $ingredients = array();
 
+    public function __construct($name, Array $ingredients)
+    {
+        $this->name = $name;
+        $this->ingredients = $ingredients;
+    }
+
+    public function __get($property)
+    {
+        return (property_exists($this, $property)) ? $this->$property : null;
+    }
+
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property))
+            $this->$property = $value;
+    }
 }
--- tests/Unit/Models/RecipeTest.php
+++ tests/Unit/Models/RecipeTest.php
@@ -4,9 +4,17 @@ namespace Unit\Models;
 
 class RecipeTest extends \PHPUnit_Framework_TestCase
 {
+    private $recipes;
+
+    public function setUp()
+    {
+        $this->recipes = json_decode(file_get_contents(dirname(dirname(__DIR__)).'/fixtures/recipes.json'));
+    }
+
     public function testInstantiate()
     {
-        $r = new \Models\Recipe();
-        $this->assertNotNull($r);
+        $r = new \Models\Recipe($this->recipes[0]->name, $this->recipes[0]->ingredients);
+        $this->assertEquals($r->name, $this->recipes[0]->name);
+        $this->assertEquals($r->ingredients, $this->recipes[0]->ingredients);
     }
 }
